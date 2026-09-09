18dd109980dbd7dae2dab672c4bb827f804890da
WenZoo1982 <wenzoo1982@proton.me>
2026-09-09 23:44:38 +0800

    修改域名配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://WenZoo1982.github.io/",
-	base: "/blog/",
+	site: "https://wenzoo.cc.cd/",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
